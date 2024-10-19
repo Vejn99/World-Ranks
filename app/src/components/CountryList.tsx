3c712c2b1f3cbb0651fb691ed8b812e35920180b
Vane <vanestoev99@gmail.com>
2024-10-19 15:47:23 +0200

    search functionality

--- app/src/components/CountryList.tsx
+++ app/src/components/CountryList.tsx
@@ -1,9 +1,32 @@
 import "../style/CountryList.css";
 import ReactLoading from "react-loading";
 import { useApiData } from "../hooks/useApiData";
+import { useState } from "react";
+import { CountryInterface } from "../hooks/CountryInterface";
 
 export const CountryList = () => {
   const { countries, loading, error } = useApiData();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (event: any) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredCountries = countries.filter((country: CountryInterface) => {
+    const nameMatch = country.name.common
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+
+    const regionMatch = country.region
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+
+    const subregionMatch = country.subregion
+      ?.toLowerCase()
+      .includes(searchTerm.toLowerCase());
+
+    return nameMatch || regionMatch || subregionMatch;
+  });
 
   return (
     <>
@@ -16,7 +39,19 @@ export const CountryList = () => {
         <div className="inner">
           <div className="search-bar d-flex align-items-center justify-content-between txt-dark">
             <p>Found {countries.length} countries</p>
-            <div>SEARCH BAR</div>
+            <div className="search-bar-inner">
+              <img
+                src="/images/Search.svg"
+                alt="search icon"
+                className="search-icon"
+              />
+              <input
+                type="text"
+                placeholder="Search by Name, Region, Subregion"
+                onChange={handleSearchChange}
+                className="search-input"
+              />
+            </div>
           </div>
           <div className="d-flex align-items-center justify-content-between mt-5">
             <div className="left-filter w-25 d-flex txt-dark">
@@ -54,17 +89,30 @@ export const CountryList = () => {
                     <p>{error}</p>
                   </div>
                 ) : (
-                  countries.map((country, index) => (
-                    <div className="country-info-row txt-light" key={index}>
-                      <div className="flag-img">
-                        <img src={country.flags.svg} alt="country flag" />
-                      </div>
-                      <h4>{country.name.common}</h4>
-                      <p>{country.population.toLocaleString()}</p>
-                      <p>{country.area.toLocaleString()}</p>
-                      <p>{country.region}</p>
-                    </div>
-                  ))
+                  <>
+                    {filteredCountries.length > 0 ? (
+                      (searchTerm ? filteredCountries : countries).map(
+                        (country, index) => (
+                          <div
+                            className="country-info-row txt-light"
+                            key={index}
+                          >
+                            <div className="flag-img">
+                              <img src={country.flags.svg} alt="country flag" />
+                            </div>
+                            <h4>{country.name.common}</h4>
+                            <p>{country.population.toLocaleString()}</p>
+                            <p>{country.area.toLocaleString()}</p>
+                            <p>{country.region}</p>
+                          </div>
+                        )
+                      )
+                    ) : (
+                      <h3 className="d-flex justify-content-center mt-5">
+                        No Countries found
+                      </h3>
+                    )}
+                  </>
                 )}
               </div>
             </div>
